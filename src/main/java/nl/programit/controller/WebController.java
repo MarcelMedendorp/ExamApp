f438defaee1e969638b3d532c9480f01aa13682a
RemondKarst <Remond@192.168.1.13>
2016-07-07 12:02:40 +0200

    Added comments to the Webcontroller Removed the errors And created logical Links to the webpages

--- src/main/java/nl/programit/controller/WebController.java
+++ src/main/java/nl/programit/controller/WebController.java
@@ -12,18 +12,27 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import eu.programit.domain.Answer;
-import eu.programit.domain.Category;
-import eu.programit.domain.Question;
-import eu.programit.domain.TestViews;
-import eu.programit.service.AnswerService;
-import eu.programit.service.CategoryService;
-import eu.programit.service.QuestionService;
+import nl.programit.domain.Answer;
+import nl.programit.domain.Category;
+import nl.programit.domain.ExamView;
+import nl.programit.domain.Question;
+import nl.programit.service.AnswerService;
+import nl.programit.service.CategoryService;
+import nl.programit.service.QuestionService;
 
-@Controller
+
+
+@Controller//zorgt ervoor dat Java deze class automatisch runt en zo de pagina's hier beneden in de gaten houd
 public class WebController {
+	
+	private static String toTheCreateNewCategoryPage = "category";
+	private static String toTheLoginPage = "login";
+	private static String toTheIndexPage = "index";
+	private static String toTheNextQuestionOfTheExamPage = "redirect:/loadExamQuestion";
+	private static String toTheAddAnswersToTheQuestionPage = "vraag";
+	private static String toTheAddExamToTheDatabasePage = "addExam";
 
-    @Autowired
+    @Autowired//zorgt ervoor dat java zelf uitzoekt hoe die onderstaand object koppelt
     CategoryService categoryService;
 
     @Autowired
@@ -32,48 +41,62 @@ public class WebController {
     @Autowired
     AnswerService answerService;
 
-    @RequestMapping("/")
+    @RequestMapping("/")//zodra je naar pagina localhost:8888/ gaat voert die automatisch deze methode uit
     public String start(){
-        return "login";
+        return toTheLoginPage;
     }
 
     @RequestMapping("/login")
     public String login (){
-        return "login";
+        return toTheLoginPage;
     }
 
     @RequestMapping("/index")
     public String index (){
-        return "index";
+        return toTheIndexPage;
     }
+
+//onderstaande methode zorgt voor het volgende:
+//als je na de pagina localhost:8888/vraag gaat, dan handeld deze methode het af en geeft aan de pagina via de variable model
+//gegevens mee aan de webpagina deze kunnen vervolgens door thymeleaf worden gebruikt, hij geeft dus een leeg object van 
+//Question mee, zodat alle velden hiervan beschikbaar zijn voor de pagina tevens haalt die alle categorien op voor het pulldown
+//menu op de pagina, zodat die is geveld met alle categorien die op dat moment in de database zitten.
 
     @RequestMapping("/vraag")
     public String createQuestion(Model model){
         model.addAttribute("question", new Question());
         model.addAttribute("categories", categoryService.findAll());
-        return "vraag";
+        return toTheAddAnswersToTheQuestionPage;
     }
-
+  //onderstaande methode zorgt voor het volgende:
+  //als je na de pagina localhost:8888/createexam gaat, dan handeld deze methode het af en geeft aan de pagina via de variable model
+  //gegevens mee aan de webpagina deze kunnen vervolgens door thymeleaf worden gebruikt, hij geeft dus een leeg object van 
+  //ExamView mee, zodat alle velden hiervan beschikbaar zijn voor de pagina op te roepen via de variablee testviews in de pagina zelf
+  //Als die klaar is gaat die naar de pagina addExam
     @RequestMapping("/createexam")
     public String createExam(Model model) {
-        model.addAttribute("testviews", new TestViews());
+        model.addAttribute("testviews", new ExamView());
 
-        return "addExam";
+        return toTheAddExamToTheDatabasePage;
     }
-    
+  //onderstaande methode zorgt voor het volgende:
+  //als je na de pagina localhost:8888/categorysave gaat, kun je via de variable category in thymeleaf de variabelen van Category
+  //bereiken en vullen als je die vervolgens op de pagina submit, dan zorgt de pagina category ervoor dat deze worden opgeslagen in de database 
     @RequestMapping("/categorysave")
     public String createCategory(Model model){
         model.addAttribute("category", new Category());
-        return "category";
+        return toTheCreateNewCategoryPage;
     }
 
-    
+  //onderstaande methode zorgt voor het volgende:
+  //als je na de pagina localhost:8888/registerQuestion gaat, geeft het POST aan dat die daar invoer verwacht
+  //op de pagina laat die alle antwoorden zien die bij de vraag horen die die via het model reeds binnen krijgt
     @RequestMapping(value = "/registerQuestion", method = RequestMethod.POST)
     String saveQuestion(Model model, @ModelAttribute("answers") ArrayList<Answer> answers){
         for(Answer x:answers) {
             System.out.println(x);
         }
-        return "redirect:/loadExamQuestion";
+        return toTheNextQuestionOfTheExamPage;
     }
 
 
